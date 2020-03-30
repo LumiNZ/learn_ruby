18de580670eae61ea6149acc2d8dbc7436e941ef
LumiDesktop <sam.stephens.nz@gmail.com>
2020-03-30 16:08:56 +1300

    Completed problem 5 & 6

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,32 @@
 class Book
-# write your code here
+
+    def initialize 
+        @title = ''
+    end
+
+    def titleize sentence
+        little_words = ["and", "over", "the", "in", "of", "a", "an"]
+        words = sentence.split(' ')
+        new_sentence = []
+        words.each do |word|
+            if not little_words.include?(word)
+                word = word.capitalize
+            end
+            new_sentence << word
+        end
+        new_word = new_sentence.join(" ")
+        new_word[0] = new_word[0].upcase
+        return new_word
+        
+    end
+
+    def title=(title)                   #Setter
+        @title =  titleize(title)
+    end
+
+    def title                           #Getter
+        @title
+    end
+
+
 end
--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,24 @@
+require 'time'
+
 class Timer
-  #write your code here
+  
+  def initialize
+    @seconds = 0
+
+  end
+
+
+  def seconds
+    @seconds
+  end
+
+  def seconds=(seconds)
+    @seconds = seconds
+  end
+
+  def time_string
+    temp_time = Time.new(0) + seconds
+    return temp_time.strftime("%H:%M:%S")
+  end
+
 end
